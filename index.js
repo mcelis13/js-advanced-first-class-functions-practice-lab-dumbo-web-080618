ae547db396572f97b7585b1dd441bde6041cb362
Madeline Celis <celiscb@vcu.edu>
2018-09-17 15:46:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name);
+  });
+};
+
+function logDriversByHometown(drivers, location){
+  drivers.forEach(function(driver){
+    if(driver.hometown === location){
+      console.log(driver.name);
+    }
+  });
+};
+
+
+function driversByRevenue(drivers){
+  var newArray = drivers.slice();
+  return newArray.sort(function(ele, nextEle){
+    return ele.revenue - nextEle.revenue;
+  });
+};
+
+function driversByName(drivers){
+  var newArray = drivers.slice();
+  return newArray.sort(function(ele, nextEle){
+    return ele['name'].localeCompare(nextEle['name']);
+  });
+};
+
+function totalRevenue(drivers){
+  return drivers.reduce(function(accumulator, nextEle){
+    return accumulator + nextEle.revenue;
+  }, 0);
+};
+
+function averageRevenue(drivers){
+  return totalRevenue(drivers)/drivers.length;
+};
